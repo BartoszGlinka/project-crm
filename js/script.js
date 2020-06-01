5ab2643197924db9d28d9087602fbb842fdbcf66
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-06-02 00:56:09 +0200

    preaper layout to RWD in style.css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
+const content = document.querySelector('.content');
 
 hamburger.addEventListener('click', function (){
   hamburger.classList.toggle('hamburger--active');
   sidebar.classList.toggle('sidebar--active');
+  content.classList.toggle('content--active');
 });
